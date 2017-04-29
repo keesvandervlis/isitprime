7cc0b862ad57322bae3e509fe633f33cd639848c
Kees van der vlis <kees@slowthinkers.net>
2017-04-29 10:05:04 +0200

    finished is it prime app

--- is it prime/ViewController.swift
+++ is it prime/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var primeTextInput: UITextField!
+    
+    @IBAction func primeButtonPressed(_ sender: Any) {
+        var primeInt : Int?
+        var isPrime = true
+        
+        primeInt = Int(primeTextInput.text!)
+        if primeInt == nil {
+            primeTextInput.text = ""
+            primeTextInput.placeholder = "Please enter a whole positive Number"
+        } else {
+            if primeInt! == 1 {
+                primeLabel.text = "\(primeInt!) is not a prime number!"
+            } else {
+                var i = 2
+                while i < primeInt! {
+                    if primeInt! % i == 0 {
+                        isPrime = false
+                    }
+                    i = i + 1
+                }
+                if isPrime == true {
+                  primeLabel.text = "\(primeInt!) is a prime number!"
+                } else {
+                  primeLabel.text = "\(primeInt!) is not a prime number!"
+                }
+            }
+            
+        }
+        
+        
+    }
+    
+    @IBOutlet weak var primeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        primeLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
